54933225f28e3cc6a9ca51d54ae45ea6b3681223
MateuszW77 <mateusz.waliszewski@gmail.com>
2019-02-24 17:05:24 +0100

    Working new, create and show method in beers_controller

--- app/controllers/beers_controller.rb
+++ app/controllers/beers_controller.rb
@@ -5,7 +5,29 @@ class BeersController < ApplicationController
   end
 
   def new
-  
+    @beer = BeerType.new
   end
-   
+
+  def create
+    @beer = BeerType.new(beer_params)
+    if @beer.save
+      flash[:notice] = "Article was succesfully created"
+      redirect_to beers_path(@beer)
+    else
+      render 'new'
+    end
+    # render plain: params[:beer].inspect
+  end
+
+  def show
+    @beer = BeerType.find(params[:id])
+  end
+
+  private
+
+    def beer_params
+      params.require(:beer_type).permit(:title, :text)
+      # params.permit(:title, :text)
+    end
+
 end
